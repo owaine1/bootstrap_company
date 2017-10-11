b7a08002e580a3da7bebf7d293762362072c12e4
Owain <owaine1@live.com>
2017-10-11 15:49:46 +0800

    Knowledge is power -- knowledge shared is power lost. 		-- Aleister Crowley

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 // AIzaSyCvv0YXd1w_XI9wnAFiUsZ9VRgV-eKefgQ
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 22.284208, lng: 114.155059};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
