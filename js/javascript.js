02c4add6011c03ce8d643c2b8e4554a8918c1a06
Owain <owaine1@live.com>
2017-10-11 16:20:02 +0800

    Things will be bright in P.M.  A cop will shine a light in your face.

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,35 @@
 // AIzaSyCvv0YXd1w_XI9wnAFiUsZ9VRgV-eKefgQ
 function initMap() {
-        var uluru = {lat: 22.284208, lng: 114.155059};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }$(document).ready(setup);
+  var uluru = {
+    lat: 22.284208,
+    lng: 114.155059
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+$(document).ready(setup);
 
-      function setup(){
-        console.log('Inside setup');
-        $('#send').click(sendData);
-      }
-      function sendData(){
-        console.log('Inside sendData');
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var comments = $('#comments').val();
-console.log('data:' + name + ', '+ email + ', ' + comments);
-      }
+function setup() {
+  console.log('Inside setup');
+  $('#send').click(sendData);
+}
+
+function sendData() {
+  console.log('Inside sendData');
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var comments = $('#comments').val();
+  console.log('data:' + name + ', ' + email + ', ' + comments);
+var contact = {
+  name: name,
+  email: email,
+  comments: comments
+};
+console.log(contact);
+}
